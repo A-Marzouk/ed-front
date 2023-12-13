3c03ead617b726d28f36399b4c8a159c13a43b15
perfect_ahmed@yahoo.com <perfect_ahmed@yahoo.com>
2023-12-13 13:38:26 -0700

    Feat: ed-frontend basic app.

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is the dashboard view</h1>
+    <RouterLink to="/">Home</RouterLink>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Greeting from '../components/Greeting.vue'
+import { RouterLink } from 'vue-router'
+
+</script>
+
+<template>
+    <div class="flex gap-12">
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+      <div class="wrapper">
+        <Greeting msg="Welcome to your Educational Dashboard!" />
+
+        <nav class="mt-8">
+          <RouterLink to="/dashboard" class="p-2" style="border: 0.5px lightgreen solid;">Dashboard</RouterLink>
+        </nav>
+      </div>
+    </div>
+</template>
